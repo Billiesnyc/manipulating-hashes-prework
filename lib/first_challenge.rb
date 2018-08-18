adb64397707449ae25e0cee2d8da5ed1091a3068
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 14:51:58 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts["Freddie Mercury"].each do |attribute, value|
-    
+    if attribute == :favorite_icecream_flavors
       :favorite_icecream_flavors.delete_if {|key, value| key >= "strawberry" } 
     end
 
