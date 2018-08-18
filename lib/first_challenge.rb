f94aff01cdf4131e0e355b77b6b204f3948d3fb4
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 15:02:09 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
     data.each do |attribute, value|
       value.each do |flavor|
          flavor.delete("strawberry")
-          
+      end    
   end
 end
   #remember to return your newly altered contacts hash!
