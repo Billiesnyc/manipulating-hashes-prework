e942013f487e073e2ec3883d13a4b3e06de891ab
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 14:52:41 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts["Freddie Mercury"].each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      :favorite_icecream_flavors.delete_if {|key, value| key >= "strawberry" } 
+      :favorite_icecream_flavors.delete_if {|attribute, value| key >= "strawberry" } 
     end
 end
   #remember to return your newly altered contacts hash!
