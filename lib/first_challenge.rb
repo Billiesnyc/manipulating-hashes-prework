dd642809a9af2d250e92f6d43358c6511b73306e
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 14:54:20 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,6 +19,7 @@ def first_challenge
     if attribute == :favorite_icecream_flavors
       :favorite_icecream_flavors.delete_if {|attribute, value| key >= "strawberry" } 
     end
+  end
 end
   #remember to return your newly altered contacts hash!
   contacts
